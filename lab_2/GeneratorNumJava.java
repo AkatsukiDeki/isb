c00c509c19cc84d828026af9cfdbb77938e18a6c
AkatsukiDeki <124888750+AkatsukiDeki@users.noreply.github.com>
2024-05-14 13:48:58 +0400

    javadoc

--- lab_2/GeneratorNumJava.java
+++ lab_2/GeneratorNumJava.java
@@ -1,12 +1,24 @@
 import java.security.SecureRandom;
 
+/**
+ * Класс GeneratorNumJava генерирует случайные байты с использованием криптографически безопасного генератора случайных чисел.
+ */
 public class GeneratorNumJava {
+    /**
+     * Метод main является точкой входа в программу. Он генерирует последовательность случайных байтов и выводит их в двоичном формате на консоль.
+     *
+     * @param args входные аргументы командной строки (не используются)
+     */
     public static void main(String[] args) {
-
         byte[] sequence = generateSecureRandomBytes();
         System.out.println(bytesToBinaryString(sequence));
     }
 
+    /**
+     * Метод generateSecureRandomBytes генерирует массив из 16 криптографически безопасных случайных байтов.
+     *
+     * @return массив случайных байтов
+     */
     private static byte[] generateSecureRandomBytes() {
         SecureRandom random = new SecureRandom();
         byte[] bytes = new byte[16];
@@ -14,6 +26,12 @@ public class GeneratorNumJava {
         return bytes;
     }
 
+    /**
+     * Метод bytesToBinaryString преобразует массив байтов в строку, представляющую их в двоичном формате.
+     *
+     * @param bytes массив байтов для преобразования
+     * @return строка, представляющая массив байтов в двоичном формате
+     */
     private static String bytesToBinaryString(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
         for (byte b : bytes) {
