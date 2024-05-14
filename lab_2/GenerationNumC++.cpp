80c639670380a06f1589d8680588ffe494f664a9
AkatsukiDeki <124888750+AkatsukiDeki@users.noreply.github.com>
2024-05-14 14:02:59 +0400

    C++ Doxygen

--- lab_2/GenerationNumC++.cpp
+++ lab_2/GenerationNumC++.cpp
@@ -2,6 +2,14 @@
 #include <random>
 #include <bitset>
 
+/*!
+ * \brief Преобразует массив байтов в строку, представляющую их в двоичном формате.
+ *
+ * \param bytes Указатель на массив байтов для преобразования.
+ * \param length Длина массива байтов.
+ * \return std::string Строка, представляющая массив байтов в двоичном формате.
+ */
+
 std::string bytesToBinaryString(const unsigned char* bytes, size_t length) {
     std::string binaryString;
     for (size_t i = 0; i < length; ++i) {
@@ -9,6 +17,14 @@ std::string bytesToBinaryString(const unsigned char* bytes, size_t length) {
     }
     return binaryString;
 }
+
+/*!
+ * \brief Точка входа программы.
+ *
+ * Программа генерирует 16 случайных байтов с использованием криптографически безопасного генератора случайных чисел и выводит их в двоичном формате на консоль.
+ *
+ * \return int Код возврата программы (0 в случае успешного завершения).
+ */
 
 int main() {
 
